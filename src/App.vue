23ca4a51fd7c9839e5ce599a4a1b721536b39f65
martinmoragab <martinmoraga23@gmail.com>
2022-08-10 23:43:52 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <h1>Product Feedback</h1>
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
