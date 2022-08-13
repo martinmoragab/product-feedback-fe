5e88a74fa4bad191766a5b04f5686d41b45ad7d3
Martin Moraga <martin_moraga@trimble.com>
2022-08-12 17:12:57 -0700

    Added Feedbacks route and types

---
+++ src/pages/Feedbacks/components/@types/index.ts
@@ -0,0 +1,15 @@
+export interface Status {
+	name: string;
+	class: string;
+	count: number;
+}
+
+export interface Feedback {
+	id: string;
+	title: string;
+	category: string;
+	details: string;
+	status: string;
+	author: string;
+	commentsCount: number;
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LogIn from '../pages/LogIn/index.vue';
 import SignUp from '../pages/SignUp/index.vue';
+import Feedbacks from '../pages/Feedbacks/index.vue';
 
 const routes = [
 	{
@@ -12,6 +13,11 @@ const routes = [
 		path: '/signup',
 		name: 'SignUp',
 		component: SignUp,
+	},
+	{
+		path: '/feedbacks',
+		name: 'Feedbacks',
+		component: Feedbacks,
 	}
 ];
 
